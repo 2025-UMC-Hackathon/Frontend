73eaada7b352347ac06d82809f01923b43a340f7
nayeong lee <lny021102@gmail.com>
2025-07-06 04:36:23 +0900

    refactor: ReactNode 받을 수 있게 header 컴포넌트 개선 완료

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,27 +1,44 @@
 import { useNavigate, useLocation } from "react-router-dom";
 import { ArrowLeft } from 'lucide-react';
+import type { ReactNode } from "react";
+
+const backComponentMap: Record<string, ReactNode> = {
+	'/signup': <span className="text-base font-bold">회원가입</span>,
+	'/community':
+		(<div className="flex flex-col items-center justify-center">
+			<p className="text-base font-bold">도란도란</p>
+			<p className="text-sm">자유게시판</p>
+		</div>),
+	'/write': <span className="text-base font-bold">글 작성하기</span>,
+	'/chat':
+		(<div className="flex items-center justify-center">
+			<p className="text-base font-bold">담담이</p>
+			<p className="text-sm text-gray-200">GPT3.5</p>
+		</div>),
+	'/mypage': <span className="text-base font-bold">마이페이지</span>,
+	'/writtedbyme':
+		(<div className="flex flex-col items-center justify-center">
+			<p className="text-base font-bold">마이페이지</p>
+			<p className="text-sm">내가 작성한 글</p>
+		</div>),
+	'/likedbyme':
+		(<div className="flex flex-col items-center justify-center">
+			<p className="text-base font-bold">마이페이지</p>
+			<p className="text-sm">공감한 글</p>
+		</div>),
+};
 
 export default function Header() {
 	const navigate = useNavigate();
 	const location = useLocation();
 	const path = location.pathname;
 
-	// splash, login 페이지에서는 Header 숨김
+	// splash, login, root 경로에서는 Header 숨김
 	if (path === '/splash' || path === '/login' || path === '/') return null;
 
-	// 경로별 텍스트
-	const backTextMap: Record<string, string> = {
-		'/signup': '회원가입',
-		'/community': '커뮤니티',
-		'/write': '글 작성하기',
-		'/chat': 'AI 채팅',
-		'/mypage': '마이페이지',
-		'/writtedbyme': '내가 작성한 글',
-		'/likedbyme': '좋아요 한 게시물',
-	};
+	const centerComponent = backComponentMap[path];
 
-	// 뒤로가기 버튼이 필요한 경로
-	if (backTextMap[path]) {
+	if (centerComponent) {
 		return (
 			<div className="bg-white p-3 flex items-center h-16">
 				<button
@@ -30,17 +47,13 @@ export default function Header() {
 				>
 					<ArrowLeft size={20} color="#000" />
 				</button>
-				<div className="w-[40%] flex items-center justify-center">{backTextMap[path]}</div>
+
+				<div className="w-[40%] flex items-center justify-center">
+					{centerComponent}
+				</div>
 			</div>
 		);
 	}
 
-	// 기본 로고 헤더 ("/"인 경우) -> null로 수정 
-	// return (
-	// 	<div className="bg-white p-3 flex justify-start items-start h-16">
-	// 		<Button width="w-auto" padding="p-0" backgroundColor="bg-0" onClick={() => { navigate('/') }}>
-	// 			<img src={logo} alt="아이콘" className="w-[70px] h-[20px]" />
-	// 		</Button>
-	// 	</div>
-	// );
+	return null;
 }
