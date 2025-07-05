18b082b9da1ab74e3bbd309f06772eff6cfdf4fc
nayeong lee <lny021102@gmail.com>
2025-07-06 04:19:29 +0900

    feat: ai chat 화면 디자인 반영 완료

--- src/pages/chatpage.tsx
+++ src/pages/chatpage.tsx
@@ -1,4 +1,7 @@
 import { useEffect, useState } from 'react';
+import ai_icon from '../assets/chat_icon.svg';
+import { ArrowUp } from 'lucide-react';
+import Button from '../components/button';
 
 interface Message {
 	sender: 'me' | 'ai';
@@ -8,16 +11,21 @@ interface Message {
 
 export default function Chat() {
 	const [messages, setMessages] = useState<Message[]>([
-		{ sender: 'me', text: '안녕하세요.', type: 'text' },
-		{ sender: 'ai', text: '...', type: 'loading' },
+		// { sender: 'me', text: '안녕하세요.', type: 'text' },
+		// { sender: 'ai', text: '...', type: 'loading' },
 	]);
 
 	useEffect(() => {
-		// 2초 뒤 loading 메시지 나오도록 임시 조치 
 		const timer = setTimeout(() => {
 			setMessages((prev) =>
 				prev.map((msg) =>
-					msg.type === 'loading' ? { sender: 'ai', text: '안녕하세요. 어떤 도움이 필요하세요?', type: 'text' } : msg
+					msg.type === 'loading'
+						? {
+							sender: 'ai',
+							text: '안녕하세요. 어떤 도움이 필요하세요?',
+							type: 'text',
+						}
+						: msg
 				)
 			);
 		}, 2000);
@@ -27,39 +35,55 @@ export default function Chat() {
 
 	return (
 		<div className="flex flex-col justify-between h-[90vh] bg-white px-4 py-2">
-			{/* 메시지 리스트 */}
-			<div className="flex-1 flex flex-col gap-2 overflow-y-auto">
-				{messages.map((msg, idx) => (
-					<div
-						key={idx}
-						className={`flex ${msg.sender === 'me' ? 'justify-end' : 'justify-start'}`}
-					>
-						<div
-							className={`px-3 py-2 rounded-xl text-sm max-w-[70%] ${msg.sender === 'me'
-								? 'bg-[#5A5261] text-white rounded-br-none'
-								: 'bg-[#EFE9F3] text-black rounded-bl-none'
-								}`}
-						>
-							{msg.type === 'loading' ? (
-								<div className="flex space-x-1 animate-pulse">
-									<span>•</span>
-									<span>•</span>
-									<span>•</span>
+			{/* 메시지 없을 때 안내 문구 */}
+			{messages.length === 0 ? (
+				<div className="flex flex-col items-center justify-center h-full">
+					<img src={ai_icon} alt="아이콘" className="w-40 h-40" />
+					<p className="text-[#000] mt-4">담담이는 대답을 잘해요!</p>
+				</div>
+			) : (
+				<>
+					{/* 메시지 리스트 */}
+					<div className="flex-1 flex flex-col gap-2 overflow-y-auto">
+						{messages.map((msg, idx) => (
+							<div
+								key={idx}
+								className={`flex items-center gap-2 ${msg.sender === 'me' ? 'justify-end' : 'justify-start'}`}
+							>
+								{msg.sender === 'ai' && (
+									<img src={ai_icon} alt="아이콘" className="w-10 h-10" />
+								)}
+								<div
+									className={`px-3 py-2 rounded-2xl text-sm font-bold max-w-[70%] ${msg.sender === 'me'
+										? 'bg-[#CDE7EC] text-[#334B4E] rounded-br-md'
+										: 'bg-[#E3E9EA] text-[#3F484A] rounded-bl-md'
+										}`}
+								>
+									{msg.type === 'loading' ? (
+										<div className="flex space-x-1 animate-pulse">
+											<span>•</span>
+											<span>•</span>
+											<span>•</span>
+										</div>
+									) : (
+										msg.text
+									)}
 								</div>
-							) : (
-								msg.text
-							)}
-						</div>
+							</div>
+						))}
 					</div>
-				))}
-			</div>
+				</>
+			)}
 
 			{/* 입력창 */}
-			<div className="border-t shadow-md px-4 py-3 flex items-center">
+			<div className="flex items-center mt-2">
 				<input
-					className="flex-1 border-none focus:outline-none text-sm text-gray-500"
-					placeholder="질문을 시작하세요."
+					className="flex-1 border-none px-5 py-2 rounded-lg focus:outline-none text-sm text-gray-500 bg-[#F5F5F5]"
+					placeholder="어떤 도움이 필요하세요?"
 				/>
+				<Button borderRadius='rounded-full' backgroundColor='bg-gray-200' width='w-auto' padding='p-2'>
+					<ArrowUp size={20} />
+				</Button>
 			</div>
 		</div>
 	);
