1eeb497580b86a1b7b8ce26d264b87ca61fa2ae3
hyewonimdang <hyehyekuk202@gmail.com>
2025-07-05 20:46:34 +0900

    design: 게시판 글 컴포넌트 형태 제작

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,47 +1,16 @@
 // 깃허브에서 이슈를 파고, dev에서 feature로 뻗어나가주세요!
 
-import { useNavigate } from 'react-router-dom';
-import Button from "../components/button";
-import TextField from '../components/TextField';
+import { BoardItem } from '../components/BoardItem';
 
 export default function Home() {
-	const navigate = useNavigate();
-
-	const handleChange = () => {
-		console.log("handle");
-	}
-	
 
 	return (
-		<div className="flex flex-col items-center justify-start p-2">
-			<p className="mb-4 text-lg">여기는 홈화면~</p>
-
-			<TextField
-				
-				value="입력하세요"
-				onChange={handleChange}
-				placeholder="이메일을 입력하세요"
-				variant="login"
-				required
-			/>
-
-			<TextField
-				label="이메일"
-				value="입력하세요"
-				onChange={handleChange}
-				placeholder="이메일을 입력하세요"
-				variant="signup"
-				required
-			/>
-
-			<Button
-				text="마이페이지"
-				width="w-30"
-				height="h-10"
-				backgroundColor="bg-green-600"
-				borderRadius="rounded-full"
-				fontColor="text-white"
-				onClick={() => navigate('/my')}
+		<div className="flex flex-col items-center justify-start">
+			<BoardItem 
+				title="Lorem ipsum (글 제목)"
+				content="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. . ."
+				createdAt="4분전"
+				nickname="닉네임"
 			/>
 		</div>
 	);
