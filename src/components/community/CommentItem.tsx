9b278f7d8c692dfbee64fae0a30635adea43e9c7
nayeong lee <lny021102@gmail.com>
2025-07-06 06:35:18 +0900

    feat: 좋아요 누르기 API 연동

---
+++ src/components/community/CommentItem.tsx
@@ -0,0 +1,41 @@
+import { MessageCircle, Trash2 } from 'lucide-react';
+
+interface CommentType {
+	id: number;
+	nickname: string;
+	time: number;
+	content: string;
+	isMine: boolean;
+}
+
+export default function CommentItem({
+	comment,
+	onReply,
+	onDelete,
+}: {
+	comment: CommentType;
+	onReply: () => void;
+	onDelete: () => void;
+}) {
+	return (
+		<div className="w-full flex flex-col items-start justify-start bg-[#F5F5F5] px-2 py-3 rounded">
+			<div className="w-full flex items-start justify-between text-sm font-semibold">
+				<div className="flex items-center">
+					{comment.nickname}
+					<span className="text-xs text-gray-500 ml-1">{comment.time}분 전</span>
+				</div>
+				<div className="flex items-center">
+					<button className="p-2 bg-transparent" onClick={onReply}>
+						<MessageCircle size={14} />
+					</button>
+					{comment.isMine && (
+						<button className="p-2 bg-transparent" onClick={onDelete}>
+							<Trash2 size={16} />
+						</button>
+					)}
+				</div>
+			</div>
+			<p className="text-sm text-black">{comment.content}</p>
+		</div>
+	);
+}
---
+++ src/components/community/ReplyItem.tsx
@@ -0,0 +1,44 @@
+import { CornerDownRight, MessageCircle, Trash2 } from 'lucide-react';
+
+interface ReplyType {
+	id: number;
+	nickname: string;
+	time: number;
+	content: string;
+	isMine: boolean;
+}
+
+export default function ReplyItem({
+	reply,
+	onReply,
+	onDelete,
+}: {
+	reply: ReplyType;
+	onReply: () => void;
+	onDelete: () => void;
+}) {
+	return (
+		<div className="w-full ml-4 mt-2 p-2 rounded flex items-start gap-2">
+			<CornerDownRight size={20} />
+			<div className="w-full flex flex-col items-start justify-start">
+				<div className="w-full flex justify-between text-sm font-semibold">
+					<div className="flex items-center">
+						{reply.nickname}
+						<span className="text-xs text-gray-500 ml-1">{reply.time}분 전</span>
+					</div>
+					<div className="flex items-center">
+						<button className="p-2 bg-transparent" onClick={onReply}>
+							<MessageCircle size={14} />
+						</button>
+						{reply.isMine && (
+							<button className="p-2 bg-transparent" onClick={onDelete}>
+								<Trash2 size={16} />
+							</button>
+						)}
+					</div>
+				</div>
+				<p className="text-sm">{reply.content}</p>
+			</div>
+		</div>
+	);
+}
