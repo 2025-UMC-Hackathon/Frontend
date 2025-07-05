33b413108275b595d332b1360ce301375e876983
nayeong lee <lny021102@gmail.com>
2025-07-05 17:36:05 +0900

    feat: React Portal을 사용하여 Modal 컴포넌트 추가

--- src/pages/chatpage.tsx
+++ src/pages/chatpage.tsx
@@ -1,7 +1,31 @@
+import Modal from "../components/modal";
+import Button from "../components/button";
+import { useState } from 'react';
+
 export default function Chat() {
+	const [open, setOpen] = useState(false);
+	const modalOpen = () => {
+		setOpen(true);
+	}
 	return (
 		<div className="flex flex-col items-center justify-start p-2">
 			여기는 AI 채팅 페이지~
+
+			<Button
+				text="모달오픈"
+				width="w-30"
+				height="h-10"
+				backgroundColor="bg-green-600"
+				borderRadius="rounded-full"
+				fontColor="text-white"
+				onClick={modalOpen}
+			/>
+			{open && (
+				<Modal onClose={() => setOpen(false)}>
+					<h2 className="text-lg font-bold">모달 제목</h2>
+					<p>모달 내용입니다.</p>
+				</Modal>
+			)}
 		</div>
 	);
 }
