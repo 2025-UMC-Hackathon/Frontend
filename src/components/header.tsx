03aa4eb43cf65da34a3ca6d0bd73a625fb215977
nayeong lee <lny021102@gmail.com>
2025-07-06 03:13:14 +0900

    fix: header 수정

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,4 @@
 import { useNavigate, useLocation } from "react-router-dom";
-import Button from "./button";
-import logo from '../assets/logo_example.svg';
 import { ArrowLeft } from 'lucide-react';
 
 export default function Header() {
@@ -9,7 +7,7 @@ export default function Header() {
 	const path = location.pathname;
 
 	// splash, login 페이지에서는 Header 숨김
-	if (path === '/splash' || path === '/login') return null;
+	if (path === '/splash' || path === '/login' || path === '/') return null;
 
 	// 경로별 텍스트
 	const backTextMap: Record<string, string> = {
@@ -27,22 +25,22 @@ export default function Header() {
 		return (
 			<div className="bg-white p-3 flex items-center h-16">
 				<button
-					className="bg-white text-xl font-basic flex items-center gap-2"
+					className="w-[30%] bg-white text-xl font-basic flex items-center gap-2"
 					onClick={() => navigate(-1)}
 				>
-					<span className="text-lg"><ArrowLeft size={20} color="#000" /></span>
-					<span>{backTextMap[path]}</span>
+					<ArrowLeft size={20} color="#000" />
 				</button>
+				<div className="w-[40%] flex items-center justify-center">{backTextMap[path]}</div>
 			</div>
 		);
 	}
 
-	// 기본 로고 헤더 ("/"인 경우)
-	return (
-		<div className="bg-white p-3 flex justify-start items-start h-16">
-			<Button width="w-auto" padding="p-0" backgroundColor="bg-0" onClick={() => { navigate('/') }}>
-				<img src={logo} alt="아이콘" className="w-[70px] h-[20px]" />
-			</Button>
-		</div>
-	);
+	// 기본 로고 헤더 ("/"인 경우) -> null로 수정 
+	// return (
+	// 	<div className="bg-white p-3 flex justify-start items-start h-16">
+	// 		<Button width="w-auto" padding="p-0" backgroundColor="bg-0" onClick={() => { navigate('/') }}>
+	// 			<img src={logo} alt="아이콘" className="w-[70px] h-[20px]" />
+	// 		</Button>
+	// 	</div>
+	// );
 }
