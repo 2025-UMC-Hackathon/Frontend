b323f3a6bf23fa13b5fe7963a6c288d242bd68c5
hyewonimdang <hyehyekuk202@gmail.com>
2025-07-05 21:10:04 +0900

    feat: 홈페이지에 dropdown 형태 추가

--- src/components/BoardItem.tsx
+++ src/components/BoardItem.tsx
@@ -11,15 +11,15 @@ export const BoardItem = ({
     createdAt,
     nickname,
 }: BoardItemProps) => {
+
+
     return (
         <div className="flex flex-col text-left bg-white w-full h-[100px] border-t border-[#D9D9D9]">
             <div className="px-[15px] py-[10px]">
                 <h2 className="text-[12px] font-semibold mb-[10px]">{title}</h2>
-
                 <p className="text-[12px] font-semibold mb-[11px] line-clamp-2">
                     {content}
                 </p>
-
                 <div className="flex items-center text-[10px] gap-[7px] text-xs text-[#9C9C9C]">
                     <span>{createdAt}</span>
                     <span>|</span>
--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,17 +1,63 @@
 // 깃허브에서 이슈를 파고, dev에서 feature로 뻗어나가주세요!
 
+import { useState } from 'react';
 import { BoardItem } from '../components/BoardItem';
+import TagDropdown from '../components/dropdown';
 
 export default function Home() {
+	const [selectedTag, setSelectedTag] = useState<string[]>([]);
+
+	// 임시
+	const DisabilityType = [
+		{ label: '시각 장애', value: 'visual' },
+		{ label: '청각 장애', value: 'hearing' },
+		{ label: '지체 장애', value: 'physical' },
+		{ label: '지적 장애', value: 'intellectual' },
+		{ label: '자폐성 장애', value: 'autism' },
+		{ label: '정신 장애', value: 'mental' },
+	];
+	// 임시
+	const Worries = [
+		{ label: '진로 선택', value: 'career_path' },
+		{ label: '취업 준비', value: 'job_hunting' },
+		{ label: '전공 적합성', value: 'major_fit' },
+		{ label: '편입/유학 고민', value: 'transfer_abroad' },
+		{ label: '학업 스트레스', value: 'study_stress' },
+		{ label: '자존감/불안', value: 'self_esteem' },
+		{ label: '대인관계', value: 'relationships' },
+		{ label: '장래에 대한 불안', value: 'future_uncertainty' },
+	];
 
 	return (
-		<div className="flex flex-col items-center justify-start">
+		<div className="flex flex-col items-start">
+			<div className="flex items-center gap-[11px] mb-[18px] ml-[18px]">
+				<TagDropdown
+				tags={DisabilityType.map(type => type.label)}
+				selectedTags={selectedTag}
+				onTagChange={setSelectedTag}
+				selectionMode="single"
+				placeholder="장애 유형"
+				/>
+
+				<TagDropdown
+					tags={Worries.map(type => type.label)}
+					selectedTags={selectedTag}
+					onTagChange={setSelectedTag}
+					selectionMode="single"
+					placeholder="고민"
+				/>
+			</div>
+			
+
+
 			<BoardItem 
 				title="Lorem ipsum (글 제목)"
 				content="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. . ."
 				createdAt="4분전"
 				nickname="닉네임"
 			/>
+
+
 		</div>
 	);
 }
