76d461ea1697fbf3ce9560c1afb22f42604924b4
nayeong lee <lny021102@gmail.com>
2025-07-05 22:59:53 +0900

    feat: 네비게이션 바 디자인 반영 완료

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,35 +1,47 @@
 import { useNavigate, useLocation } from 'react-router-dom';
 import { MessageSquareText, MessageCircle, User } from 'lucide-react';
+import family from '../assets/nav_family.svg';
 
 // 기존 Footer가 NaviBar의 역할입니다.
 export default function Footer() {
 	const navigate = useNavigate();
 	const location = useLocation();
 
-	const isActive = (path: string) => location.pathname === path ? 'bg-[#D9D9D9]' : '';
+	const isActive = (path: string) => location.pathname === path ? 'bg-[#CDE7EC]' : '';
 
 	return (
-		<div className="fixed bottom-0 left-1/2 -translate-x-1/2 w-[400px] bg-white p-3 text-center z-50 border-t flex justify-around items-center">
+		<div
+			className="fixed bottom-0 left-1/2 -translate-x-1/2 w-[400px] bg-[#E9EFF0] p-1 text-center z-50 border-t
+			flex justify-around items-center
+			rounded-tl-lg rounded-tr-lg shadow-[0_-4px_10px_-2px_rgba(0,0,0,0.1)]">
+
 			<div
 				onClick={() => navigate('/')}
-				className={`flex flex-col items-center cursor-pointer p-2 rounded ${isActive('/')}`}
+				className={`flex flex-col items-center cursor-pointer p-2`}
 			>
-				<MessageSquareText className="w-6 h-6" />
-				<span className="text-sm mt-1">커뮤니티</span>
+				<div className={`flex items-center cursor-pointer p-2 rounded-full ${isActive('/')}`}>
+					{/* <MessageSquareText className="w-6 h-6" /> */}
+					<img src={family} alt="아이콘" className="w-6 h-6" />
+				</div>
+				<span className="text-sm mt-1 text-[#3F484A] font-bold">도란도란</span>
 			</div>
 			<div
 				onClick={() => navigate('/chat')}
-				className={`flex flex-col items-center cursor-pointer p-2 rounded ${isActive('/chat')}`}
+				className={`flex flex-col items-center cursor-pointer p-2`}
 			>
-				<MessageCircle className="w-6 h-6" />
-				<span className="text-sm mt-1">AI 채팅</span>
+				<div className={`flex items-center cursor-pointer p-2 rounded-full ${isActive('/chat')}`}>
+					<MessageCircle className="w-6 h-6" />
+				</div>
+				<span className="text-sm mt-1 text-[#3F484A] font-bold">대화</span>
 			</div>
 			<div
 				onClick={() => navigate('/my')}
-				className={`flex flex-col items-center cursor-pointer p-2 rounded ${isActive('/my')}`}
+				className={`flex flex-col items-center cursor-pointer p-2`}
 			>
-				<User className="w-6 h-6" />
-				<span className="text-sm mt-1">저장한 꿀팁</span>
+				<div className={`flex items-center cursor-pointer p-2 rounded-full ${isActive('/my')}`}>
+					<User className="w-6 h-6" />
+				</div>
+				<span className="text-sm mt-1 text-[#3F484A] font-bold">마이페이지</span>
 			</div>
 		</div>
 	);
